2b09c428ff6516673161a9a624cd92cf590a4059
sky <adsky0309@gmail.com>
2024-01-03 14:47:34 +0900

    d

--- gadgets.py
+++ gadgets.py
@@ -5,25 +5,25 @@ from fastapi import Request
 gadget_router = APIRouter()
 
 from starlette.responses import HTMLResponse
-gadget_template = Jinja2Templates(directory='templates/gadgets/')
+gadget_template = Jinja2Templates(directory='templates/')
 
 @gadget_router.get('/buttons', response_class=HTMLResponse)
 async def gadget_button(request:Request) :
-    return gadget_template.TemplateResponse(name='buttons.html'
+    return gadget_template.TemplateResponse(name='gadgets/buttons.html'
                                         ,context={'request':request})
 
 @gadget_router.get('/cards', response_class=HTMLResponse)
 async def gadget_card(request:Request):
-    return gadget_template.TemplateResponse(name='cards.html'
+    return gadget_template.TemplateResponse(name='gadgets/cards.html'
                                             ,context={'request':request})
 
 @gadget_router.get('/colors', response_class=HTMLResponse)
 async def gadget_color(request:Request):
-    return gadget_template.TemplateResponse(name='colors.html'
+    return gadget_template.TemplateResponse(name='gadgets/colors.html'
                                             ,context={'request':request})
 
 @gadget_router.get('/containers', response_class=HTMLResponse)
 async def gadget_container(request:Request):
-    return gadget_template.TemplateResponse(name='containers.html'
+    return gadget_template.TemplateResponse(name='gadgets/containers.html'
                                             ,context={'request':request})
 
--- positionings.py
+++ positionings.py
@@ -5,24 +5,24 @@ from fastapi import Request
 positioning_router = APIRouter()
 
 from starlette.responses import HTMLResponse
-positioning_template = Jinja2Templates(directory='templates/positionings/')
+positioning_template = Jinja2Templates(directory='templates/')
 
 @positioning_router.get('/forms', response_class=HTMLResponse)
 async def positioning_form(request:Request) :
-    return positioning_template.TemplateResponse(name='forms.html'
+    return positioning_template.TemplateResponse(name='positionings/forms.html'
                                         ,context={'request':request})
 
 @positioning_router.get('/grids', response_class=HTMLResponse)
 async def positioning_grid(request:Request):
-    return positioning_template.TemplateResponse(name='grids.html'
+    return positioning_template.TemplateResponse(name='positionings/grids.html'
                                             ,context={'request':request})
 
 @positioning_router.get('/standard', response_class=HTMLResponse)
 async def positioning_standard(request:Request):
-    return positioning_template.TemplateResponse(name='standard.html'
+    return positioning_template.TemplateResponse(name='positionings/standard.html'
                                             ,context={'request':request})
 
 @positioning_router.get('/tables', response_class=HTMLResponse)
 async def positioning_table(request:Request):
-    return positioning_template.TemplateResponse(name='tables.html'
+    return positioning_template.TemplateResponse(name='positionings/tables.html'
                                             ,context={'request':request})
